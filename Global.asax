627b8c53518b51b946dad0df7e1ef02ce82211e8
Berina Dedovic <43812807+brnddvc@users.noreply.github.com>
2018-12-09 14:07:34 +0100

    Add files via upload

---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="getfitEF.MvcApplication" Language="C#" %>
---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(getfitEF.Startup))]
+namespace getfitEF
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
